e76b346888bba6a5d2b817b8f271afea104ccba0
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-06-04 08:53:54 -0500

    quitando warnings

--- src/test/java/unfv/edu/pe/service/ProductServiceImplTest.java
+++ src/test/java/unfv/edu/pe/service/ProductServiceImplTest.java
@@ -1,19 +1,17 @@
 package unfv.edu.pe.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import unfv.edu.pe.model.Product;
 import unfv.edu.pe.repository.ProductRepository;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductServiceImplTest {
 	
+	@SuppressWarnings("unused")
 	@Autowired
 	private ProductRepository productRepository;	
 	
